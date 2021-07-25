acfd95a99737e79c533ab1226892409547229956
Lean277 <leanfernandez97@gmail.com>
2021-07-25 15:08:13 -0300

    Creacion de la clase auto y añadieron conceptos basicos

---
+++ RepasoJava/src/poo/Auto.java
@@ -0,0 +1,19 @@
+package poo;
+
+public class Auto {
+	String color;
+	int peso;
+	int motor;
+	int ancho;
+	int largo;
+	int ruedas;
+	
+	public Auto () {
+		ruedas = 4;
+		largo = 2000;
+		ancho = 300;
+		motor = 1600;
+		peso = 500;
+		color = "Azul";
+	}
+}
