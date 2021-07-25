2dbb6f8baa515e7dbe50800a458a08be253d58d9
Lean277 <leanfernandez97@gmail.com>
2021-07-25 15:08:38 -0300

    Pruebas basicas con un objeto creado

---
+++ RepasoJava/src/poo/UsoAuto.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class UsoAuto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Auto auto1 = new Auto ();
+		
+		System.out.println("Es un auto de color " + auto1.color + ", con un peso de " + auto1.peso+ " kilos.");
+	}
+
+}
