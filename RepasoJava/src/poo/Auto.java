45784d3d52da1884e6f54f1000d7ab93c5d1a91e
Lean277 <leanfernandez97@gmail.com>
2021-07-29 23:57:10 -0300

    Added the "set" and "get" methods of the attributes.

--- RepasoJava/src/poo/Auto.java
+++ RepasoJava/src/poo/Auto.java
@@ -2,18 +2,92 @@ package poo;
 
 public class Auto {
 	String color;
-	int peso;
-	int motor;
-	int ancho;
-	int largo;
-	int ruedas;
+	private int pesoPlataforma;
+	private int motor;
+	private int ancho;
+	private int largo;
+	private int ruedas;
+	private int pesoFinal;
+	private boolean asientosCuero, climatizador;
 	
+	public int getPesoPlataforma () {
+		return pesoPlataforma;
+	}
+	
+	public String getColor () {
+		return color;
+	}
+	
+	public int getMotor() {
+		return motor;
+	}
+
+	public void setMotor(int motor) {
+		this.motor = motor;
+	}
+
+	public int getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+
+	public int getLargo() {
+		return largo;
+	}
+
+	public void setLargo(int largo) {
+		this.largo = largo;
+	}
+
+	public int getRuedas() {
+		return ruedas;
+	}
+
+	public void setRuedas(int ruedas) {
+		this.ruedas = ruedas;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void setPesoPlataforma(int peso) {
+		this.pesoPlataforma = peso;
+	}
+	
+	
+	public int getPesoFinal() {
+		return pesoFinal;
+	}
+
+	public void setPesoFinal(int pesoFinal) {
+		this.pesoFinal = pesoFinal;
+	}
+
+	public boolean isAsientosCuero() {
+		return asientosCuero;
+	}
+
+	public void setAsientosCuero(boolean asientosCuero) {
+		this.asientosCuero = asientosCuero;
+	}
+
+	public boolean isClimatizador() {
+		return climatizador;
+	}
+
+	public void setClimatizador(boolean climatizador) {
+		this.climatizador = climatizador;
+	}
+
 	public Auto () {
 		ruedas = 4;
 		largo = 2000;
 		ancho = 300;
 		motor = 1600;
-		peso = 500;
-		color = "Azul";
+		pesoPlataforma = 500;;
 	}
 }
