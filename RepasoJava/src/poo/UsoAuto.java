2fdd57e6652e1700e0b2cfe75a7acfae3b9a48f4
Lean277 <leanfernandez97@gmail.com>
2021-08-06 11:42:09 -0300

    Usos basicos de un objeto creado

--- RepasoJava/src/poo/UsoAuto.java
+++ RepasoJava/src/poo/UsoAuto.java
@@ -1,12 +1,29 @@
 package poo;
 
+import java.util.Scanner;
+
 public class UsoAuto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner (System.in);
 		Auto auto1 = new Auto ();
-		
-		System.out.println("Es un auto de color " + auto1.color + ", con un peso de " + auto1.peso+ " kilos.");
+		System.out.print("Ingrese un color: ");
+		String dato = entrada.nextLine();
+		auto1.setColor(dato);
+		System.out.println(auto1.datosGenerales());
+		System.out.println("Tiene asientos de cuero?");
+		System.out.println("1: Si");
+		System.out.println("2: No");
+		dato = entrada.nextLine();
+		switch (dato) {
+			case "1": auto1.setAsientosCuero(true);
+			break;
+			case "2": auto1.setAsientosCuero(false);
+			break;
+			
+		}
+		System.out.println(auto1.datoAsientos());
 	}
 
 }
